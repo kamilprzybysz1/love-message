0cf66e0059ebf1379d60d476eb5377e6931c352e
kamilprzybysz1 <kamilox200098@gmail.com>
2025-03-02 13:51:14 +0100

    Fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function LoveMessage() {
         <h1 className="text-5xl font-bold text-red-500">I love you</h1>
         <img 
           src="/heart.png" 
-          alt="Heart" '
+          alt="Heart"
 	  width:"360"
 	  height"360"
           className="w-32 h-32 mx-auto mt-4"
