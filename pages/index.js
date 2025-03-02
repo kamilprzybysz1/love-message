d68bd035a07c9421c40f63b07920678ddbf19dfe
kamilprzybysz1 <kamilox200098@gmail.com>
2025-03-02 13:48:50 +0100

    Fixed image

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 export default function LoveMessage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100">
-      <h1 className="text-5xl font-bold text-red-500">I love you</h1>
-      <img 
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Red_Heart.svg/1024px-Red_Heart.svg.png" 
-        alt="Heart" 
-        className="w-32 h-32 mt-4" 
-      />
+    <div className="flex items-center justify-center min-h-screen bg-pink-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-red-500">I love you</h1>
+        <img 
+          src="/heart.png" 
+          alt="Heart" '
+	  width:"360"
+	  height"360"
+          className="w-32 h-32 mx-auto mt-4"
+        />
+      </div>
     </div>
   );
 }
