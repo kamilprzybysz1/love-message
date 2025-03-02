630e5063035a03d70064c51583377da510730845
kamilprzybysz1 <kamilox200098@gmail.com>
2025-03-02 14:00:41 +0100

    Fix 34

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
-      <div className="text-center">
+      <div className="flex flex-col items-center justify-center text-center">
         <h1 className="text-5xl font-bold text-white">I love you</h1>
         <img 
           src="/heart.png" 
           alt="Heart" 
           width="360" 
           height="360"
-          className="mx-auto mt-4"
+          className="mt-4"
         />
       </div>
     </div>
